797e8d4d7b03296399cd5f3e2f81062471b174d7
jremondo <jandro@MacBook-Pro-de-Alejandro.local>
2021-06-01 20:38:17 +0200

    resolve issues

--- ansible/grafana/scripts/exportKubeLogs.sh
+++ ansible/grafana/scripts/exportKubeLogs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exportar logs en HunExp.csv
-kubectl logs hunter-exporter-scd8q > /tmp/HunExp.csv
+kubectl logs hunter-exporter-hjj8q > /tmp/HunExp.csv
 
 # Preparar hunterExporter para ingestar a la BD
 nl -s "," /tmp/HunExp.csv > /tmp/hunterExporter.csv
